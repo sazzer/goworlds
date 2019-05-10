10d31131dcfb500989b42d98d836e9c25fd29add
Graham Cox <graham@grahamcox.co.uk>
2019-05-10 07:59:04 +0100

    Support for logging

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,12 +1,19 @@
+#[macro_use]
+extern crate log;
+
 use dotenv;
 use ::goworlds_service::settings::Settings;
+use log4rs;
 
 /// The main entrypoint to start the entire application
 fn main() {
   dotenv::dotenv().ok();
   let s = Settings::new();
 
+  log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
   println!("{:?}", s);
 
+  info!("Starting Service...");
   ::goworlds_service::start_service().unwrap();
 }
