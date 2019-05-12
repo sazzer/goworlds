9c10e1892d6e81ab7ae37a225cf5e3a65cac71d7
Graham Cox <graham@grahamcox.co.uk>
2019-05-12 10:44:02 +0100

    Added Postgres to the docker-compose stack

---
+++ service/docker/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CMD="dockerize -timeout 30s"
+
+if [ ! -z "$DOCKERIZE_WAIT" ]; then
+  CMD="$CMD $DOCKERIZE_WAIT"
+fi
+
+echo Starting...
+echo cmd: $CMD
+
+$CMD /goworlds/goworlds-service
