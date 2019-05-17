8a122e95da305b8cb7f50b8483847e15884e85ea
Graham Cox <graham@grahamcox.co.uk>
2019-05-17 12:27:14 +0100

    Docker Compose support

--- service/src/main/docker/start.sh
+++ service/src/main/docker/start.sh
@@ -4,4 +4,15 @@ if [ -f application.properties.template ]; then
     envsubst < application.properties.template > application.properties
 fi
 
-java -jar service.jar
+CMD="dockerize -timeout 30s"
+
+if [ ! -z "$DOCKERIZE_WAIT" ]; then
+  CMD="$CMD $DOCKERIZE_WAIT"
+fi
+
+CMD="$CMD java -jar service.jar"
+
+echo Starting...
+echo cmd: $CMD
+
+$CMD
