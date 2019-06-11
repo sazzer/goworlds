c273e2a9567d52f1507c11ca1b9471156d6cc5c5
Graham Cox <graham@grahamcox.co.uk>
2019-06-11 06:59:45 +0100

    Introduced Semantic UI

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 
 function App() {
   return (
-    <div className="App">
-    Hello
-    </div>
+      <Container>
+        Hello, World
+      </Container>
   );
 }
 
