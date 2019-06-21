5f1841fd7d5ce0101b4a9146231d7ab6399db7a7
Graham Cox <graham@grahamcox.co.uk>
2019-06-21 16:01:54 +0100

    Added Formik to the Login form

--- webapp/src/ui/homepage/login/Login.tsx
+++ webapp/src/ui/homepage/login/Login.tsx
@@ -1,6 +1,9 @@
 import React, {FunctionComponent, useState} from 'react';
 import {Button, Form} from "semantic-ui-react";
 import {useTranslation} from "react-i18next";
+import * as Yup from "yup";
+import {Formik} from "formik";
+import {FormikErrorMessage} from "../../common/FormikErrorMessage";
 
 /** The props that the Login area needs */
 type LoginProps = {
@@ -14,37 +17,59 @@ type LoginProps = {
  */
 export const Login: FunctionComponent<LoginProps> = ({email, onCancel}) => {
     const { t } = useTranslation();
-    const [password, setPassword] = useState("");
+    const [submitting, setSubmitting] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+
+    const schema = Yup.object().shape({
+        email: Yup.string()
+            .required(t('loginArea.email.errors.required')),
+        password: Yup.string()
+            .required(t('loginArea.password.errors.required')),
+    });
+
+    const doSubmit = (email: string, password: string) => {
+        setSubmitting(true);
+    };
 
     return (
-        <Form>
-            <Form.Field>
-                <label>
-                    {t('loginArea.email.label')}
-                </label>
-                <input placeholder={t('loginArea.email.placeholder')}
-                       name="email"
-                       type="text"
-                       value={email}
-                       readOnly />
-                <Form.Field>
-                    <label>
-                        {t('loginArea.password.label')}
-                    </label>
-                    <input placeholder={t('loginArea.password.placeholder')}
-                           name="password"
-                           type="password"
-                           value={password}
-                           onChange={e => setPassword(e.target.value)}
-                           autoFocus/>
-                </Form.Field>
-                <Button type="submit" primary>
-                    {t('loginArea.submit.login')}
-                </Button>
-                <Button type="reset" negative onClick={onCancel}>
-                    {t('loginArea.submit.cancel')}
-                </Button>
-            </Form.Field>
-        </Form>
+        <Formik initialValues={{email: email, password: ''}}
+                validationSchema={schema}
+                onSubmit={(values) => doSubmit(values.email, values.password)}>
+            {({values, isValid, errors, touched, handleSubmit, handleChange, handleBlur}) =>
+                <Form onSubmit={handleSubmit} error={!isValid || error !== undefined} loading={submitting}>
+                    <Form.Field required>
+                        <label>
+                            {t('loginArea.email.label')}
+                        </label>
+                        <Form.Input fluid
+                                    placeholder={t('loginArea.email.placeholder')}
+                                    name="email"
+                                    type="text"
+                                    value={values.email}
+                                    readOnly />
+                    </Form.Field>
+                    <Form.Field required>
+                        <label>
+                            {t('loginArea.password.label')}
+                        </label>
+                        <Form.Input fluid
+                                    placeholder={t('loginArea.password.placeholder')}
+                                    name="password"
+                                    type="password"
+                                    value={values.password}
+                                    error={touched.password && errors.password !== undefined}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur} />
+                        <FormikErrorMessage name="password" />
+                    </Form.Field>
+                    <Button type="submit" primary>
+                        {t('loginArea.submit.register')}
+                    </Button>
+                    <Button type="reset" negative onClick={onCancel}>
+                        {t('loginArea.submit.cancel')}
+                    </Button>
+                </Form>
+            }
+        </Formik>
     );
 };
