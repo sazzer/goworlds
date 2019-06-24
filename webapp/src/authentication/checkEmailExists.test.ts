5ca136b04d4cb469c148e71a195ac37e2c40aa98
Graham Cox <graham@grahamcox.co.uk>
2019-06-24 17:18:06 +0100

    Tests for the User Registration

--- webapp/src/authentication/checkEmailExists.test.ts
+++ webapp/src/authentication/checkEmailExists.test.ts
@@ -1,105 +1,103 @@
 import * as testSubject from './checkEmailExists';
-import {expectSaga} from "redux-saga-test-plan";
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 
 /** The Mock Axios layer */
 const mockAxios = new MockAdapter(axios);
 
-describe('checkEmailExists', () => {
-    it('Generates the correct action', () => {
-        const callback = jest.fn();
+it('Generates the correct action', () => {
+    const callback = jest.fn();
 
-        expect(testSubject.checkEmailExists('graham@grahamcox.co.uk', callback)).toEqual({
+    expect(testSubject.checkEmailExists('graham@grahamcox.co.uk', callback)).toEqual({
+        type: 'Authentication/checkEmailExists',
+        payload: {
+            email: 'graham@grahamcox.co.uk',
+            callback,
+        },
+    });
+});
+
+describe('checkEmailExistsSaga', () => {
+    it('Acts correctly when the email exists', (done) => {
+        mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(200, {
+            exists: true
+        }, {
+            'content-type': 'application/json'
+        });
+
+        const action = {
             type: 'Authentication/checkEmailExists',
             payload: {
                 email: 'graham@grahamcox.co.uk',
-                callback,
+                callback: function(status, err) {
+                    expect(status).toEqual(true);
+                    expect(err).toBeUndefined();
+                    done();
+                },
             },
-        });
+        };
+
+        testSubject.checkEmailExistsSaga(action);
     });
 
-    describe('checkEmailExistsSaga', () => {
-        it('Acts correctly when the email exists', (done) => {
-            mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(200, {
-                exists: true
-            }, {
-                'content-type': 'application/json'
-            });
-
-            const action = {
-                type: 'Authentication/checkEmailExists',
-                payload: {
-                    email: 'graham@grahamcox.co.uk',
-                    callback: function(status, err) {
-                        expect(status).toEqual(true);
-                        expect(err).toBeUndefined();
-                        done();
-                    },
-                },
-            };
-
-            testSubject.checkEmailExistsSaga(action);
+    it('Acts correctly when the email is unknown', (done) => {
+        mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(200, {
+            exists: false
+        }, {
+            'content-type': 'application/json'
         });
 
-        it('Acts correctly when the email is unknown', (done) => {
-            mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(200, {
-                exists: false
-            }, {
-                'content-type': 'application/json'
-            });
-
-            const action = {
-                type: 'Authentication/checkEmailExists',
-                payload: {
-                    email: 'graham@grahamcox.co.uk',
-                    callback: function(status, err) {
-                        expect(status).toEqual(false);
-                        expect(err).toBeUndefined();
-                        done();
-                    },
+        const action = {
+            type: 'Authentication/checkEmailExists',
+            payload: {
+                email: 'graham@grahamcox.co.uk',
+                callback: function(status, err) {
+                    expect(status).toEqual(false);
+                    expect(err).toBeUndefined();
+                    done();
                 },
-            };
+            },
+        };
 
-            testSubject.checkEmailExistsSaga(action);
+        testSubject.checkEmailExistsSaga(action);
+    });
+
+    it('Acts correctly when the API call fails', (done) => {
+        mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(400, {
+            error: 'Oops'
+        }, {
+            'content-type': 'application/json'
         });
 
-        it('Acts correctly when the API call fails', (done) => {
-            mockAxios.onGet('/emails/graham%40grahamcox.co.uk').reply(400, {
-                error: 'Oops'
-            }, {
-                'content-type': 'application/json'
-            });
-
-            const action = {
-                type: 'Authentication/checkEmailExists',
-                payload: {
-                    email: 'graham@grahamcox.co.uk',
-                    callback: function(status, err) {
-                        expect(err.toString()).toEqual('Error: An error occurred making a request');
-                        done();
-                    },
+        const action = {
+            type: 'Authentication/checkEmailExists',
+            payload: {
+                email: 'graham@grahamcox.co.uk',
+                callback: function(status, err) {
+                    expect(err.toString()).toEqual('Error: An error occurred making a request');
+                    done();
                 },
-            };
+            },
+        };
 
-            testSubject.checkEmailExistsSaga(action);
-        });
+        testSubject.checkEmailExistsSaga(action);
+    });
 
-        it('Acts correctly when the Network Request fails', (done) => {
-            mockAxios.onGet('/emails/graham%40grahamcox.co.uk').networkError();
+    it('Acts correctly when the Network Request fails', (done) => {
+        mockAxios.onGet('/emails/graham%40grahamcox.co.uk').networkError();
 
-            const action = {
-                type: 'Authentication/checkEmailExists',
-                payload: {
-                    email: 'graham@grahamcox.co.uk',
-                    callback: function(status, err) {
-                        expect(err.toString()).toEqual('Error: Network Error');
-                        done();
-                    },
+        const action = {
+            type: 'Authentication/checkEmailExists',
+            payload: {
+                email: 'graham@grahamcox.co.uk',
+                callback: function(status, err) {
+                    expect(err.toString()).toEqual('Error: Network Error');
+                    done();
                 },
-            };
+            },
+        };
 
-            testSubject.checkEmailExistsSaga(action);
-        });
+        testSubject.checkEmailExistsSaga(action);
     });
 });
+
---
+++ webapp/src/authentication/register.test.ts
@@ -0,0 +1,236 @@
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { expectSaga } from 'redux-saga-test-plan';
+import * as testSubject from "./register";
+import {buildSaga} from "../redux";
+import {ProblemError} from "../api";
+
+/** The Mock Axios layer */
+const mockAxios = new MockAdapter(axios);
+
+it('Generates the correct action', () => {
+    const callback = jest.fn();
+
+    expect(testSubject.register('graham@grahamcox.co.uk', 'Graham', 'password', callback)).toEqual({
+        type: 'Authentication/register',
+        payload: {
+            email: 'graham@grahamcox.co.uk',
+            name: 'Graham',
+            password: 'password',
+            callback,
+        },
+    });
+});
+
+describe('registerSaga', () => {
+    it('Acts correctly when the registration succeeds', () => {
+        mockAxios.onPost('/users').reply(200, {
+            response: 'here' // The actual response is ignored
+        }, {
+            'content-type': 'application/json'
+        });
+
+        const callback = jest.fn();
+
+        const action = {
+            type: 'Authentication/register',
+            payload: {
+                email: 'graham@grahamcox.co.uk',
+                name: 'Graham',
+                password: 'password',
+                callback,
+            },
+        };
+
+        return expectSaga(buildSaga('Authentication/register', testSubject.registerSaga))
+            .put({
+                type: 'Authentication/register_STARTED',
+                input: {
+                    email: 'graham@grahamcox.co.uk',
+                    name: 'Graham',
+                    password: 'password',
+                    callback,
+                }
+            })
+            .put({
+                type: 'Authentication/register_SUCCEEDED',
+                payload: {
+                    response: 'here'
+                },
+                input: {
+                    email: 'graham@grahamcox.co.uk',
+                    name: 'Graham',
+                    password: 'password',
+                    callback,
+                }
+            })
+            .put({
+                type: 'Authentication/register_FINISHED',
+                input: {
+                    email: 'graham@grahamcox.co.uk',
+                    name: 'Graham',
+                    password: 'password',
+                    callback,
+                }
+            })
+            .dispatch(action)
+            .run();
+    });
+
+    describe('Acts correctly when registration fails', () => {
+        it('Duplicate Email Address', () => {
+            mockAxios.onPost('/users').reply(409, {
+                type: 'tag:goworlds,2019:users/problems/duplicate-email-address',
+                title: 'Duplicate Email Address',
+                status: 409
+            }, {
+                'content-type': 'application/problem+json'
+            });
+
+            const callback = jest.fn();
+
+            const action = {
+                type: 'Authentication/register',
+                payload: {
+                    email: 'graham@grahamcox.co.uk',
+                    name: 'Graham',
+                    password: 'password',
+                    callback,
+                },
+            };
+
+            return expectSaga(buildSaga('Authentication/register', testSubject.registerSaga))
+                .put({
+                    type: 'Authentication/register_STARTED',
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .put({
+                    type: 'Authentication/register_FAILED',
+                    payload: new ProblemError('Duplicate Email Address',
+                        409,
+                        'tag:goworlds,2019:users/problems/duplicate-email-address',
+                        {}),
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .put({
+                    type: 'Authentication/register_FINISHED',
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .dispatch(action)
+                .run();
+        });
+        it('Network Error', () => {
+            mockAxios.onPost('/users').networkError();
+            const callback = jest.fn();
+
+            const action = {
+                type: 'Authentication/register',
+                payload: {
+                    email: 'graham@grahamcox.co.uk',
+                    name: 'Graham',
+                    password: 'password',
+                    callback,
+                },
+            };
+
+            return expectSaga(buildSaga('Authentication/register', testSubject.registerSaga))
+                .put({
+                    type: 'Authentication/register_STARTED',
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .put({
+                    type: 'Authentication/register_FAILED',
+                    payload: new Error('Network Error'),
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .put({
+                    type: 'Authentication/register_FINISHED',
+                    input: {
+                        email: 'graham@grahamcox.co.uk',
+                        name: 'Graham',
+                        password: 'password',
+                        callback,
+                    }
+                })
+                .dispatch(action)
+                .run();
+        });
+    });
+});
+
+describe('registerFailedSaga', () => {
+    it('Triggers the callback with the correct error', () => {
+        const callback = jest.fn();
+        const err = new Error('Network Error');
+
+        testSubject.registerFailedSaga({
+            type: 'Authentication/register_FAILED',
+            payload: err,
+            input: {
+                email: 'graham@grahamcox.co.uk',
+                name: 'Graham',
+                password: 'password',
+                callback,
+            }
+        });
+
+        expect(callback).toBeCalledTimes(1);
+        expect(callback).toBeCalledWith(err);
+    });
+});
+
+describe('registerSucceededSaga', () => {
+    it('Triggers the Authenticate action', () => {
+        const callback = jest.fn();
+
+        const action = {
+            type: 'Authentication/register_SUCCEEDED',
+            payload: {
+                response: 'here'
+            },
+            input: {
+                email: 'graham@grahamcox.co.uk',
+                name: 'Graham',
+                password: 'password',
+                callback,
+            }
+        };
+
+        return expectSaga(buildSaga('Authentication/register_SUCCEEDED', testSubject.registerSucceededSaga))
+            .put({
+                type: 'Authentication/authenticate',
+                payload: {
+                    email: 'graham@grahamcox.co.uk',
+                    password: 'password',
+                    callback,
+                }
+            })
+            .dispatch(action)
+            .run();
+    });
+});
