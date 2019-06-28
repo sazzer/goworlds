ddcda1b1267ad2c9c28fdbd125f6fd9537a03953
Graham Cox <graham@grahamcox.co.uk>
2019-06-28 09:40:12 +0100

    Moved from Jest to Cucumber

--- e2e/src/browser.ts
+++ e2e/src/browser.ts
@@ -1,30 +1,39 @@
+import {AfterAll, BeforeAll} from 'cucumber';
 import {Builder, By, ThenableWebDriver, WebElement} from "selenium-webdriver";
 import {BasePage} from "./pages/BasePage";
 
 /** The actual web driver */
 let driver: ThenableWebDriver;
 
-beforeAll(() => {
-    console.log('Opening Web Driver');
-    driver = new Builder()
-        .forBrowser(process.env.SELENIUM_BROWSER as string)
-        .build();
+BeforeAll(() => {
+    const browser = process.env.SELENIUM_BROWSER as string;
 
+    console.log('Loading browser: ' + browser);
+    driver = new Builder()
+        .forBrowser(browser)
+        .build();
 });
 
-afterAll(async () => {
-    console.log('Closing Web Driver');
+AfterAll(async () => {
+    console.log('Killing browser');
     await driver.quit();
 });
+
 
 /**
  * Open the web browser to the given URL
  * @param url the URL
+ */
+export async function openPage(url: string) {
+    await driver.get(url);
+}
+
+/**
+ * Build a page model using the Web Driver
  * @param pageBuilder The builder for the root page model
  * @return the page model
  */
-export async function openPage<T extends BasePage>(url: string, pageBuilder: (base: WebElement) => T) : Promise<T> {
-    await driver.get(url);
+export async function getPageModel<T extends BasePage>(pageBuilder: (base: WebElement) => T) : Promise<T> {
     const rootElement: WebElement = await driver.findElement(By.id("root"));
 
     return pageBuilder(rootElement);
